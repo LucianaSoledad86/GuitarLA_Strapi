eca0797cb9838e2e533a2bec6fb715072d5f904d
Luciana <lucianadiaz2007@hotmail.com>
2022-08-05 10:52:05 -0300

    deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4920755ef2178cc4154601be73f46c63'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '87465c5d-58a7-4935-b1e5-3e6b9933555f'
+};
